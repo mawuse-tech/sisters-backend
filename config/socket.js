d499911bd47baf11e0acc53e75bcef0c6a3c6e88
mawuse-tech <akpekemawuse1@gmail.com>
2025-11-13 13:12:47 +0000

    dist folder

--- config/socket.js
+++ config/socket.js
@@ -4,7 +4,7 @@ import Chat from "../models/chatSchema.js";
 export const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://sisters-backend.onrender.com",
       credentials: true,
     },
   });
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // }));
 const allowedOrigins = [
   "http://localhost:5173",        // local dev
-  "https://sist.netlify.app"  // Netlify deployment
+  "https://sisters-backend.onrender.com"  // Netlify deployment
 ];
 
 app.use(cors({
