91cf39d02405b5aa7328a182b3f6b5a1cd66e6fa
mawuse-tech <akpekemawuse1@gmail.com>
2025-11-17 11:30:21 +0000

    added the dist folder

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -160,7 +160,8 @@ export const forgotPassword = async (req, res, next) => {
         const resetToken = user.createResetPasswordToken();
         await user.save({ validateBeforeSave: false });
 
-        const resetPasswordLink = `${req.protocol}://process.env.FRONTEND_URL/resetpassword/${resetToken}`;
+        const resetPasswordLink = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
         const subject = "Password Reset Request";
         const html = `
       <p>You requested a password reset.</p>
